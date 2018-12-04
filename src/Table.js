f54392fb4156324cf80291cb743c9e96999d2ebb
zigoval <rouby.v@hotmail.com>
2018-12-04 08:38:46 +0100

    feat/tic tac is working correctly, next step, check is winner

--- src/Table.js
+++ src/Table.js
@@ -1,6 +1,39 @@
 import React, { Component } from "react";
 import unicorn1 from "./images/unicorn1.jpeg";
 import unicorn2 from "./images/unicorn2.jpeg";
+import empty from "./images/empty.png";
+
+class Unicase extends Component {
+  fonction = cell => {
+    if (cell === "X") {
+      return <img src={unicorn1} alt="Licone <3" width="99%" height="98%" />;
+    } else if (cell === "O") {
+      return (
+        <img
+          src={unicorn2}
+          alt="une autre Licone <3"
+          width="99%"
+          height="98%"
+        />
+      );
+    } else {
+      return (
+        <img
+          src={empty}
+          alt="une autre Licone <3"
+          width="100px"
+          height="100px"
+        />
+      );
+    }
+  };
+  render() {
+    const board = this.props.state.board;
+    const cell = this.props.cell;
+    const playerOne = this.props.state.p1TurnYouOn;
+    return this.fonction(cell);
+  }
+}
 
 class Table extends Component {
   constructor(props) {
@@ -18,20 +51,28 @@ class Table extends Component {
     this.handleChange.bind(this);
     this.handleclick.bind(this);
   }
+
+  fonction = playerOne => {
+    if (playerOne) {
+      this.setState({ p1TurnYouOn: !this.state.p1TurnYouOn });
+      return "X";
+    } else {
+      this.setState({ p1TurnYouOn: !this.state.p1TurnYouOn });
+      return "O";
+    }
+  };
+
   handleclick = (x, y) => () => {
     const newBoard = this.state.board.map((row, rx) =>
       row.map((cell, cy) =>
         x === rx && y === cy
-          ? this.state.p1TurnYouOn
-            ? "X"
-            : "O"
-          : cell === ""
-          ? ""
+          ? cell === ""
+            ? this.fonction(this.state.p1TurnYouOn)
+            : cell
           : cell
       )
     );
     this.setState({ board: newBoard });
-    this.setState({ p1TurnYouOn: !this.state.p1TurnYouOn });
   };
 
   createTable = () => {
@@ -42,16 +83,7 @@ class Table extends Component {
             <tr>
               {row.map((cell, y) => (
                 <td className="case" onClick={this.handleclick(x, y)}>
-                  {this.state.board[x][y] === "" ||
-                    (this.state.board[x][y] === "X" ? (
-                      <img src={unicorn1} className="imgUni" alt="unicorn <3" />
-                    ) : (
-                      <img
-                        src={unicorn2}
-                        className="imgUni"
-                        alt="an other unicorn <3"
-                      />
-                    ))}
+                  <Unicase x={x} y={y} state={this.state} cell={cell} />
                 </td>
               ))}
             </tr>
